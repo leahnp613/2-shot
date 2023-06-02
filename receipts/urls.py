e32e75d37b2a4edc1cb48122887f6171a8336cff
leahnp613 <leahnagarpowers@gmail.com>
2023-06-02 14:47:59 -0400

    Feature 12 complete!

--- receipts/urls.py
+++ receipts/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
-from receipts.views import receipt_list, create_receipt
+from receipts.views import (
+    receipt_list,
+    create_receipt,
+    account_list,
+    category_list,
+)
 
 
 # app_name = "receipts"
 urlpatterns = [
     path("", receipt_list, name="home"),
+    path("accounts/", account_list, name="account_list"),
     path("create/", create_receipt, name="create_receipt"),
+    path("categories/", category_list, name="category_list"),
 ]
--- receipts/views.py
+++ receipts/views.py
@@ -29,3 +29,19 @@ def create_receipt(request):
     }
 
     return render(request, "create.html", context)
+
+
+@login_required
+def category_list(request):
+    category_list = ExpenseCategory.objects.filter(owner=request.user.id)
+    context = {"category_list": category_list}
+
+    return render(request, "category_list.html", context)
+
+
+@login_required
+def account_list(request):
+    account_list = Account.objects.filter(owner=request.user.id)
+    context = {"account_list": account_list}
+
+    return render(request, "account_list.html", context)
